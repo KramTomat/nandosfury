eb7c66f7e45516594f9613a1d39e6c9a29803ab0
Chris Pickford <chrispickford@gmail.com>
2019-09-17 11:59:10 +0100

    Create nandosfury.js

---
+++ nandosfury.js
@@ -0,0 +1,6 @@
+(function(window, document, undefined) {
+  const btnChoose = document.getElementById('btnChoose');
+  const txtResult = document.getElementById('txtResult');
+  
+  console.log(btnChoose, txtResult);
+})(window, document);
