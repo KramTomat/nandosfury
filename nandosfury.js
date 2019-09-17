4943bc38d9204f0bf46fe8cab2943c708be4fbc6
Chris Pickford <chrispickford@gmail.com>
2019-09-17 12:47:21 +0100

    Update nandosfury.js

--- nandosfury.js
+++ nandosfury.js
@@ -3,4 +3,12 @@
   const txtResult = document.getElementById('txtResult');
   
   console.log(btnChoose, txtResult);
+  
+  fetch('names.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(JSON.stringify(myJson));
+    });
 })(window, document);
